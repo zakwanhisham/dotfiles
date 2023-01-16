5295fba5eed8048551ffb86908e68e20d8b05617
Zakwan <hishamzakwan@gmail.com>
2023-01-16 17:36:45 +0800

    Add trouble.nvim

--- lvim/config.lua
+++ lvim/config.lua
@@ -50,15 +50,15 @@ lvim.builtin.telescope.defaults.mappings = {
 
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
--- lvim.builtin.which_key.mappings["t"] = {
---   name = "+Trouble",
---   r = { "<cmd>Trouble lsp_references<cr>", "References" },
---   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
---   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
--- }
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Trouble",
+  r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+}
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -184,12 +184,16 @@ lvim.builtin.lualine.sections.lualine_y = {
 -- Additional Plugins
 lvim.plugins = {
   { "kdheepak/lazygit.nvim" },
-  { "catppuccin/nvim", as = "catppuccin" },
+  -- { "catppuccin/nvim", as = "catppuccin" },
   {
     "folke/todo-comments.nvim",
     config = function()
       require("todo-comments").setup {}
     end
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
   },
   {
     "windwp/nvim-ts-autotag",
