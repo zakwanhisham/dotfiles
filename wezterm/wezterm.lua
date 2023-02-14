6b501d07b457c9c13841d808a1266dd2a5855797
Zakwan <hishamzakwan@gmail.com>
2023-02-14 15:52:32 +0800

    Update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,10 @@
 local wezterm = require("wezterm")
 
 local padding = {
-    left = "0cell",
-    right = "0cell",
-    top = "0cell",
-    bottom = "0cell",
+  left = "0cell",
+  right = "0cell",
+  top = "0cell",
+  bottom = "0cell",
 }
 
 -- A helper function for fallback fonts
@@ -14,86 +14,86 @@ local function font_with_fallback(name, params)
 end
 
 return {
-    bidi_enabled = true,
-    bidi_direction = "AutoLeftToRight",
-    color_scheme = "tokyonight_night",
-    font = font_with_fallback({
+  bidi_enabled = true,
+  bidi_direction = "AutoLeftToRight",
+  color_scheme = "tokyonight_night",
+  font = font_with_fallback({
+    family = "JetBrains Mono NF",
+    harfbuzz_features = {
+      "zero",
+    },
+  }),
+  font_rules = {
+    {
+      intensity = "Bold",
+      font = font_with_fallback({
         family = "JetBrains Mono NF",
         harfbuzz_features = {
-            "zero",
+          "zero",
         },
+        weight = "Medium",
+      }),
+    },
+    {
+      italic = true,
+      intensity = "Bold",
+      font = font_with_fallback({
+        family = "JetBrains Mono NF",
+        weight = "Medium",
+        italic = true,
+      }),
+    },
+    {
+      italic = true,
+      font = font_with_fallback({
+        family = "JetBrains Mono NF",
+        weight = "Regular",
+        italic = true,
+      }),
+    },
+  },
+  initial_cols = 1920,
+  initial_rows = 1080,
+  use_dead_keys = false,
+  window_padding = padding,
+  use_resize_increment = true,
+  hide_tab_bar_if_only_one_tab = true,
+  selection_word_boundary = " \t\n{}[]()\"'`,;:@",
+  line_height = 1.00,
+  font_size = 10,
+  window_background_opacity = 1.00,
+  bold_brightens_ansi_colors = true,
+  tab_bar_at_bottom = true,
+  show_tab_index_in_tab_bar = true,
+  -- fancy_tab_bar
+  fancy_tab_bar = true,
+  max_tab_width = 8,
+  window_frame = {
+    font = font_with_fallback({
+      family = "JetBrains Mono NF",
+      harfbuzz_features = {
+        "zero",
+      },
     }),
-    font_rules = {
-        {
-            intensity = "Bold",
-            font = font_with_fallback({
-                family = "JetBrains Mono NF",
-                harfbuzz_features = {
-                    "zero",
-                },
-                weight = "Medium",
-            }),
-        },
-        {
-            italic = true,
-            intensity = "Bold",
-            font = font_with_fallback({
-                family = "JetBrains Mono NF",
-                weight = "Medium",
-                italic = true,
-            }),
-        },
-        {
-            italic = true,
-            font = font_with_fallback({
-                family = "JetBrains Mono NF",
-                weight = "Regular",
-                italic = true,
-            }),
-        },
+    font_size = 9,
+    active_titlebar_bg = "#1a1b26",
+    inactive_titlebar_bg = "#0e0e14",
+  },
+  colors = {
+    tab_bar = {
+      inactive_tab_edge = "#33467c",
+      active_tab = {
+        bg_color = "#0e0e14",
+        fg_color = "#c0caf5",
+      },
+      inactive_tab = {
+        bg_color = "#1a1b26",
+        fg_color = "#c0caf5",
+      },
+      new_tab = {
+        bg_color = "#1a1b26",
+        fg_color = "#c0caf5",
+      },
     },
-    initial_cols = 1920,
-    initial_rows = 1080,
-    use_dead_keys = false,
-    window_padding = padding,
-    use_resize_increment = true,
-    hide_tab_bar_if_only_one_tab = true,
-    selection_word_boundary = " \t\n{}[]()\"'`,;:@",
-    line_height = 1.00,
-    font_size = 10,
-    window_background_opacity = 1.00,
-    bold_brightens_ansi_colors = true,
-    tab_bar_at_bottom = true,
-    show_tab_index_in_tab_bar = false,
-    -- fancy_tab_bar
-    fancy_tab_bar = true,
-    max_tab_width = 8,
-    window_frame = {
-        font = font_with_fallback({
-            family = "JetBrains Mono NF",
-            harfbuzz_features = {
-                "zero",
-            },
-        }),
-        font_size = 9,
-        active_titlebar_bg = "#1a1b26",
-        inactive_titlebar_bg = "#0e0e14",
-    },
-    colors = {
-        tab_bar = {
-            inactive_tab_edge = "#33467c",
-            active_tab = {
-                bg_color = "#0e0e14",
-                fg_color = "#c0caf5",
-            },
-            inactive_tab = {
-                bg_color = "#1a1b26",
-                fg_color = "#c0caf5",
-            },
-            new_tab = {
-                bg_color = "#1a1b26",
-                fg_color = "#c0caf5",
-            },
-        },
-    },
+  },
 }
