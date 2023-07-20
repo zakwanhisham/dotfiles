225a025a4b7a7e415dd79ccf07c9d305856d929f
zakwan hisham <hishamzakwan@gmail.com>
2023-07-20 17:26:53 +0800

    Lualine and Harpoon update

--- lvim/lua/user/lualine.lua
+++ lvim/lua/user/lualine.lua
@@ -1,21 +1,35 @@
 local components = require("lvim.core.lualine.components")
 
+local diagnostics = {
+	"diagnostics",
+	sources = { "nvim_diagnostic" },
+	sections = { "error", "warn" },
+	symbols = { error = " ", warn = " " },
+	colored = false,
+	always_visible = true,
+}
+
+local location = {
+	"location",
+	padding = 0,
+}
+
 lvim.builtin.lualine.options.theme = "auto"
 lvim.builtin.lualine.sections.lualine_a = { "mode" }
 lvim.builtin.lualine.sections.lualine_b = { "branch" }
 lvim.builtin.lualine.sections.lualine_c = {
 	components.diff,
-	components.diagnostics,
+	diagnostics,
 }
 lvim.builtin.lualine.sections.lualine_x = {
 	components.lsp,
 	components.spaces,
-	"encoding",
+	components.filetype,
 }
 lvim.builtin.lualine.sections.lualine_y = {
-	components.filetype,
 	components.treesitter,
+	location,
 }
 lvim.builtin.lualine.sections.lualine_z = {
-	components.location,
+	"progress",
 }
