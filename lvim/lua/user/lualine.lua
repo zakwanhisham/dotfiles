3f18655263d8045326d55eb768450ffd570626b6
zakwan hisham <hishamzakwan@gmail.com>
2023-07-20 11:29:02 +0800

    tweak some config

--- lvim/lua/user/lualine.lua
+++ lvim/lua/user/lualine.lua
@@ -1,23 +1,21 @@
--- lvim.builtin.lualine.style = "default"
-
 local components = require("lvim.core.lualine.components")
 
 lvim.builtin.lualine.options.theme = "auto"
 lvim.builtin.lualine.sections.lualine_a = { "mode" }
 lvim.builtin.lualine.sections.lualine_b = { "branch" }
 lvim.builtin.lualine.sections.lualine_c = {
-	-- components.filename,
-	components.diff,
 	components.diagnostics,
 }
 lvim.builtin.lualine.sections.lualine_x = {
-	components.lsp,
+	components.diff,
 	components.spaces,
+	"encoding",
+	components.filetype,
+	components.treesitter,
 }
 lvim.builtin.lualine.sections.lualine_y = {
-	components.treesitter,
-	components.filetype,
+	components.location,
 }
 lvim.builtin.lualine.sections.lualine_z = {
-	components.location,
+	components.progress,
 }
