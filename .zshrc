eb1e9c4fd94ecd2c6a0db242121e56b10b427577
zakwan hisham <hishamzakwan@gmail.com>
2023-08-15 10:48:35 +0800

    Update, I guess

--- .zshrc
+++ .zshrc
@@ -109,11 +109,9 @@ case ":$PATH:" in
 esac
 # pnpm end
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/home/zakwan/.local/share/google-cloud-sdk/path.zsh.inc' ]; then . '/home/zakwan/.local/share/google-cloud-sdk/path.zsh.inc'; fi
-
-# The next line enables shell command completion for gcloud.
-if [ -f '/home/zakwan/.local/share/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/zakwan/.local/share/google-cloud-sdk/completion.zsh.inc'; fi
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Starship
 eval "$(starship init zsh)"
--- lvim/lua/user/formatter.lua
+++ lvim/lua/user/formatter.lua
@@ -1,59 +1,55 @@
 -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
-	{
-		command = "eslint",
-		filetypes = {
-			"javascript",
-			"javascriptreact",
-			"typescript",
-			"typescriptreact",
-		},
-	},
-	{
-		command = "prettierd",
-		filetypes = {
-			-- "javascript",
-			-- "javascriptreact",
-			-- "typescript",
-			-- "typescriptreact",
-			"css",
-			"html",
-			"markdown",
-			"jsonc",
-			"json",
-			"yaml",
-		},
-	},
-	{
-		command = "gofmt",
-		filetypes = { "go" },
-	},
-	{
-		command = "clang-format",
-		filetypes = {
-			"cpp",
-			"c",
-		},
-	},
-	{
-		command = "rustfmt",
-		filetypes = { "rust" },
-	},
-	{
-		command = "stylua",
-		filetypes = { "lua" },
-	},
-	{
-		command = "shfmt",
-		filetypes = { "sh" },
-	},
-	{
-		command = "taplo",
-		filetypes = { "toml" },
-	},
-	{
-		command = "nimpretty",
-		filetypes = { "nim" },
-	},
+    {
+        command = "eslint",
+        filetypes = {
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+        },
+    },
+    {
+        command = "prettierd",
+        filetypes = {
+            -- "javascript",
+            -- "javascriptreact",
+            -- "typescript",
+            -- "typescriptreact",
+            "css",
+            "html",
+            "markdown",
+            "jsonc",
+            "json",
+            "yaml",
+        },
+    },
+    {
+        command = "gofmt",
+        filetypes = { "go" },
+    },
+    {
+        command = "clang-format",
+        filetypes = {
+            "cpp",
+            "c",
+        },
+    },
+    {
+        command = "rustfmt",
+        filetypes = { "rust" },
+    },
+    {
+        command = "stylua",
+        filetypes = { "lua" },
+    },
+    {
+        command = "shfmt",
+        filetypes = { "sh" },
+    },
+    {
+        command = "taplo",
+        filetypes = { "toml" },
+    },
 })
