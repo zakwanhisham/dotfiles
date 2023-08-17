6c1a8e195c93e900362f30f666c123f82ddfce30
zakwan hisham <hishamzakwan@gmail.com>
2023-08-17 17:43:40 +0800

    Clean the files code

--- lvim/lua/user/formatter.lua
+++ lvim/lua/user/formatter.lua
@@ -53,3 +53,28 @@ formatters.setup({
         filetypes = { "toml" },
     },
 })
+
+-- set additional linters
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({
+    {
+        command = "eslint",
+        filetypes = {
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+        },
+    },
+    {
+        command = "golangci-lint",
+        filetypes = { "go" },
+    },
+    {
+        command = "clang-check",
+        filetypes = {
+            "cpp",
+            "c",
+        },
+    },
+})
