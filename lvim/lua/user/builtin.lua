f655ea9665890b08140cb27dd5ff4961c5819361
zakwan hisham <hishamzakwan@gmail.com>
2023-09-18 16:06:35 +0800

    feat: Update the config
    Remove the alpha plugins in `lvim`, replace `Code` with `VSCodium` in `i3`, add the
    `rulers` in `helix`

--- lvim/lua/user/builtin.lua
+++ lvim/lua/user/builtin.lua
@@ -2,7 +2,7 @@
 lvim.log.level = "info"
 lvim.format_on_save.enabled = false
 
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.alpha.active = false
+-- lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.bufferline.active = false
 lvim.builtin.nvimtree.active = false
