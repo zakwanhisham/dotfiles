66f673e3c536ae892d5e27a2803109a380427833
zakwan hisham <hishamzakwan@gmail.com>
2024-04-16 00:10:03 +0800

    Update somethang

--- copy.sh
+++ copy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# set -xe
+set -xe
 
 # Destination folder
 dotfile="/home/zakwan/.dotfiles"
@@ -26,7 +26,7 @@ errors=false
 
 # Copy files form each source folder
 for folder in "${source_folders[@]}"; do
-	cp -vr "$folder" "$dotfile"
+	cp -r "$folder" "$dotfile"
 
 	# Check if an error occurred during copy
 	if [ $? -ne 0 ]; then
@@ -39,4 +39,5 @@ done
 # Display success message if no errors occurred
 if [ "$errors" = false ]; then
 	echo "Files copied successfully!"
+	paru --pacman pacman -Qqe >~/.dotfiles/pkglist.txt
 fi
--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -18,7 +18,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 --[[ File Explorer ]]
-vim.keymap.set("n", "<leader>e", "<cmd>Explore<cr>", { desc = "File Explorer" })
+-- vim.keymap.set("n", "<leader>e", "<cmd>Explore<cr>", { desc = "File Explorer" })
 
 --[[ Split window ]]
 vim.keymap.set("n", "<leader>\\", "<cmd>vsplit<cr>", { desc = "Split Vertical" })
@@ -79,7 +79,7 @@ vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<cr>", { desc = "Neogit" })
 --[[ Diagnostic keymaps ]]
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
--- vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 -- vim.keymap.set("n", "<leader>d", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 --[[ Trouble ]]
