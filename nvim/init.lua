66857f937622c75ebda874c56be058af76c65bc5
zakwan hisham <hishamzakwan@gmail.com>
2024-06-02 16:49:30 +0800

    update lazy.nvim bootstrap

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,7 +5,7 @@ require "core.autocommand"
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
 ---@diagnostic disable-next-line: undefined-field
-if not vim.uv.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
     vim.fn.system {
         "git",
         "clone",
