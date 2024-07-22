06efea502c243200b5dffd18d73efd91aacef7d9
zakwan hisham <hishamzakwan@gmail.com>
2024-07-22 15:47:16 +0800

    Add nvim-highlight-color

---
+++ nvim/lua/plugins/hightlight-color.lua
@@ -0,0 +1,18 @@
+return {
+    "brenoprata10/nvim-highlight-colors",
+    event = "VeryLazy",
+    config = function()
+        require("nvim-highlight-colors").setup {
+            render = "background",
+            enable_hex = true,
+            enable_short_hex = true,
+            enable_rgb = true,
+            enable_hsl = true,
+            enable_var_usage = true,
+            enable_named_colors = true,
+            enable_tailwind = true,
+            exlude_filetype = {},
+            exclude_buftype = {},
+        }
+    end,
+}
