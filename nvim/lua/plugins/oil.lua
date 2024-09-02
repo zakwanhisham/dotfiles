9bd7a3bc4010ef76b744e0610d4b56ac7e6299a4
zakwan hisham <hishamzakwan@gmail.com>
2024-09-02 11:36:43 +0800

    reorder, single letter and birthtime

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,6 +1,10 @@
 return {
     "stevearc/oil.nvim",
     dependencies = { require("mini.icons").setup {} },
+    cmd = { "Oil" },
+    keys = {
+        { "<leader>-", "<cmd>Oil<cr>", desc = "Oil" },
+    },
     opts = {
         default_file_explorer = true,
         watch_for_changes = true,
@@ -8,14 +12,11 @@ return {
             "icon",
             "permissions",
             "size",
-            "mtime",
+            "birthtime",
         },
         view_options = {
             show_hidden = true,
             case_insensitive = true,
         },
-    },
-    keys = {
-        { "<leader>-", "<cmd>Oil<cr>", desc = "Oil" },
     },
 }
