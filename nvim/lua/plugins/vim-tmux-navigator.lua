46224effcc3183aad0dd67e1f0da3acc9f6eb948
zakwan hisham <hishamzakwan@gmail.com>
2024-09-15 22:57:01 +0800

    Add vim-tmux-navigator

---
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,17 @@
+return {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+        "TmuxNavigateLeft",
+        "TmuxNavigateDown",
+        "TmuxNavigateUp",
+        "TmuxNavigateRight",
+        "TmuxNavigatePrevious",
+    },
+    keys = {
+        { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>", desc = "Navigate Left" },
+        { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", desc = "Navigate Down" },
+        { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", desc = "Navigate Up" },
+        { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>", desc = "Navigate Right" },
+        { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>", desc = "Navigate Previous" },
+    },
+}
