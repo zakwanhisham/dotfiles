0d69a1e3a2c533ae3cfc9ac5f75b9287ee23519b
zakwan hisham <hishamzakwan@gmail.com>
2024-10-16 15:55:25 +0800

    Remove unnecessary linting

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -12,7 +12,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     }
 end
 
----@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
