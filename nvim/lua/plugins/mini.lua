84e7526dd5433517db325941b5a05bd55210f5e8
zakwan hisham <hishamzakwan@gmail.com>
2024-10-16 14:12:39 +0800

    Update mini.lua

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -24,6 +24,17 @@ return {
                 { mode = "n", keys = "g" },
                 { mode = "x", keys = "g" },
 
+                -- Marks
+                { mode = "n", keys = "'" },
+                { mode = "n", keys = "`" },
+                { mode = "x", keys = "'" },
+                { mode = "x", keys = "`" },
+
+                -- Registers
+                { mode = "n", keys = '"' },
+                { mode = "x", keys = '"' },
+                { mode = "i", keys = "<C-r>" },
+                { mode = "c", keys = "<C-r>" },
                 -- Window commands
                 { mode = "n", keys = "<C-w>" },
 
@@ -42,6 +53,8 @@ return {
                 { mode = "n", keys = "<Leader>g", desc = "Git" },
                 require("mini.clue").gen_clues.builtin_completion(),
                 require("mini.clue").gen_clues.g(),
+                require("mini.clue").gen_clues.marks(),
+                require("mini.clue").gen_clues.registers(),
                 require("mini.clue").gen_clues.windows(),
                 require("mini.clue").gen_clues.z(),
             },
