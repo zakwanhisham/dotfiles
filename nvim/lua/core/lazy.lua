7d45b60daf376d994bbd319848de134d74c79607
zakwan hisham <hishamzakwan@gmail.com>
2024-10-16 15:53:52 +0800

    Move lazy.nvim to core.lazy

---
+++ nvim/lua/core/lazy.lua
@@ -0,0 +1,57 @@
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+---@diagnostic disable-next-line: undefined-field
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system {
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    }
+end
+
+---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins", {
+    defaults = {
+        lazy = true,
+    },
+    install = {
+        colorscheme = { "default" },
+    },
+    change_detection = {
+        enabled = true,
+        notify = false,
+    },
+    pkg = {
+        enabled = false,
+    },
+    rocks = {
+        enabled = false,
+    },
+    performance = {
+        rtp = {
+            disabled_plugins = {
+                "2html_plugin",
+                "getscript",
+                "getscriptPlugin",
+                "gzip",
+                "logiPat",
+                "osc52",
+                "rplugin",
+                "rrhelper",
+                "spellfile",
+                "tar",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "vimball",
+                "zip",
+                "zipPlugin",
+            },
+        },
+    },
+})
