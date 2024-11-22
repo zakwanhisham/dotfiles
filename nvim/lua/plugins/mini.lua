70e4e473323fddb86836709f341ea164df84f173
zakwan hisham <hishamzakwan@gmail.com>
2024-11-23 01:43:59 +0800

    Remove mini.pairs

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -70,19 +70,6 @@ return {
             },
         }
 
-        -- require("mini.pairs").setup {
-        --     modes = { insert = true, command = true, terminal = false },
-        --     -- skip autopair when next character is one of these
-        --     skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
-        --     -- skip autopair when the cursor is inside these treesitter nodes
-        --     skip_ts = { "string" },
-        --     -- skip autopair when next character is closing pair
-        --     -- and there are more closing pairs than opening pairs
-        --     skip_unbalanced = true,
-        --     -- better deal with markdown code blocks
-        --     markdown = true,
-        -- }
-
         require("mini.ai").setup { n_lines = 500 }
         require("mini.indentscope").setup { options = { try_as_border = false } }
         require("mini.move").setup {}
