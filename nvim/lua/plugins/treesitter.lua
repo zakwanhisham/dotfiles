b051bbb0feb560187746b1141e8c064bcbddf7be
zakwan hisham <hishamzakwan@gmail.com>
2024-12-06 20:04:21 +0800

    Disable TS highlighting and vim regex highlighting
    - Disable TS highlighting when file is more than ~100 KB
    - Disable additional vim regex highlighting

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -11,7 +11,18 @@ return {
         ---@diagnostic disable-next-line: missing-fields
         require("nvim-treesitter.configs").setup {
             -- Add languages to be installed here that you want installed for treesitter
-            highlight = { enable = true },
+            highlight = {
+                enable = true,
+                disable = function(lang, buf)
+                    local max_filesize = 100 * 1024 -- 100 KB
+                    ---@diagnostic disable-next-line: undefined-field
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        return true
+                    end
+                end,
+                additional_vim_regex_highlighting = false,
+            },
             indent = { enable = true },
             ensure_installed = {
                 "c",
