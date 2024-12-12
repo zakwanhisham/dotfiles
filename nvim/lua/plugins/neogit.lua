66d99fe2c4db18d67037cf7ed31f167a3f4c66bb
zakwan hisham <hishamzakwan@gmail.com>
2024-12-12 19:52:15 +0800

    Use lazygit and remove neogit

--- nvim/lua/plugins/neogit.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "NeogitOrg/neogit",
-    cmd = { "Neogit" },
-    keys = {
-        { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
-    },
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = true,
-}
