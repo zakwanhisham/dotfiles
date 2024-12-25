eed34d342884ab9f854e713e5e89b84aaedf9cc6
zakwan hisham <hishamzakwan@gmail.com>
2024-12-25 21:31:11 +0800

    some formatting

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -3,15 +3,10 @@ return {
     event = { "BufReadPost", "BufWritePost", "BufNewFile" },
     opts = {
         on_attach = function(bufnr)
-            vim.keymap.set(
-                "n",
-                "<leader>gp",
-                require("gitsigns").preview_hunk,
-                { buffer = bufnr, desc = "Preview git hunk" }
-            )
+            vim.keymap.set("n", "<leader>gp", require("gitsigns").preview_hunk,
+                { buffer = bufnr, desc = "Preview git hunk" })
             vim.keymap.set("n", "<leader>gb", require("gitsigns").blame, { buffer = bufnr, desc = "Git blame" })
 
-            -- don't override the built-in and fugitive keymaps
             local gs = package.loaded.gitsigns
             vim.keymap.set({ "n", "v" }, "]c", function()
                 if vim.wo.diff then
