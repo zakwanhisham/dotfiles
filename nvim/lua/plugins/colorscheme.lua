1f2842b3439607b7930009965b6649ffca76a958
zakwan hisham <hishamzakwan@gmail.com>
2025-02-22 00:48:29 +0800

    Change to use Gruvbox Dark Hard

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,13 @@
 return {
-    "rebelot/kanagawa.nvim",
+    "sainnhe/gruvbox-material",
     lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme "kanagawa-dragon"
-    end,
+        vim.g.gruvbox_material_background = "hard"
+        vim.g.gruvbox_material_foreground = "original"
+        vim.g.gruvbox_material_dim_inactive_windows = 1
+        vim.g.gruvbox_material_ui_contrast = "high"
+
+        vim.cmd.colorscheme "gruvbox-material"
+    end
 }
