967de31a99b6b935d190f47bd55abb9fa9883764
zakwan hisham <hishamzakwan@gmail.com>
2025-02-22 11:47:56 +0800

    Use back kanagawa.nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,8 @@
 return {
-    "sainnhe/gruvbox-material",
+    "rebelot/kanagawa.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-        vim.g.gruvbox_material_background = "hard"
-        vim.g.gruvbox_material_foreground = "original"
-        vim.g.gruvbox_material_dim_inactive_windows = 1
-        vim.g.gruvbox_material_ui_contrast = "high"
-
-        vim.cmd.colorscheme "gruvbox-material"
+        vim.cmd.colorscheme "kanagawa-dragon"
     end
 }
