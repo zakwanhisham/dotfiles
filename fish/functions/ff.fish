53281a3990b9f6de52a6f8c87a8244cf5ba725f5
zakwan hisham <hishamzakwan@gmail.com>
2025-02-24 16:23:57 +0800

    No color

--- fish/functions/ff.fish
+++ fish/functions/ff.fish
@@ -1,7 +1,7 @@
 function ff -d "Find files and cd into it"
     set selected_dir (fd --hidden --type directory . "$HOME" | fzf --header "Directory Selection" --height 40%)
     if test -n "$selected_dir"
-        printf "Moving to \033[34m%s\033[0m\n" "$selected_dir"
+        printf "Moving to %s\n" "$selected_dir"
         cd "$selected_dir"
     else
         echo "No directory selected."
