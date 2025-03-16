3894acc65ad37a354143c936773a23ae28d7a205
zakwan hisham <hishamzakwan@gmail.com>
2025-03-17 02:01:10 +0800

    Update blink config

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -4,20 +4,13 @@ return {
     dependencies = "rafamadriz/friendly-snippets",
     version = "*",
     opts = {
-        keymap = {
-            preset = "enter",
-        },
+        keymap = { preset = "enter" },
         appearance = {
             use_nvim_cmp_as_default = true,
             nerd_font_variant = "mono",
         },
         completion = {
-            list = {
-                selection = {
-                    preselect = false,
-                    auto_insert = false
-                }
-            },
+            list = { selection = { preselect = false, auto_insert = false } },
             menu = {
                 draw = {
                     columns = {
@@ -27,13 +20,10 @@ return {
                     treesitter = { "lsp" },
                 },
             },
-            documentation = {
-                auto_show = true,
-                auto_show_delay_ms = 200,
-            },
+            documentation = { auto_show = true, auto_show_delay_ms = 200 },
+            ghost_text = { enabled = false },
         },
-        sources = {
-            default = { "lsp", "path", "snippets", "buffer" },
-        },
+        sources = { default = { "lsp", "path", "snippets", "buffer" } },
+        signature = { enabled = false },
     },
 }
