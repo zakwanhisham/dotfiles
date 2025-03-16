b39dcd79244d982b457b9498de27eb610fb2760f
zakwan hisham <hishamzakwan@gmail.com>
2025-03-17 02:26:01 +0800

    Clean up file

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -3,19 +3,12 @@ return {
     lazy = false,
     dependencies = { require("mini.icons").setup {} },
     cmd = "Oil",
-    keys = {
-        { "<leader>-", "<cmd>Oil<cr>", desc = "Oil" },
-    },
+    keys = { "<leader>-", "<cmd>Oil<cr>", desc = "Oil" },
     opts = {
         default_file_explorer = true,
         watch_for_changes = true,
         columns = { "permissions", "size", "birthtime", "icon" },
-        view_options = {
-            show_hidden = true,
-            case_insensitive = true,
-        },
-        keymaps = {
-            ["q"] = "actions.close",
-        },
+        view_options = { show_hidden = true, case_insensitive = true },
+        keymaps = { ["q"] = "actions.close" },
     },
 }
