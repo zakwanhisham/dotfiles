bf6f1a001df211f01acae7d86dd7f9b4f544906e
zakwan hisham <hishamzakwan@gmail.com>
2025-03-17 02:25:54 +0800

    Clean up file

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -3,11 +3,12 @@ return {
     event = { "BufReadPost", "BufWritePost", "BufNewFile" },
     opts = {
         on_attach = function(bufnr)
+            local gs = package.loaded.gitsigns
+
             vim.keymap.set("n", "<leader>gp", require("gitsigns").preview_hunk,
                 { buffer = bufnr, desc = "Preview git hunk" })
             vim.keymap.set("n", "<leader>gb", require("gitsigns").blame, { buffer = bufnr, desc = "Git blame" })
 
-            local gs = package.loaded.gitsigns
             vim.keymap.set({ "n", "v" }, "]c", function()
                 if vim.wo.diff then
                     return "]c"
@@ -17,6 +18,7 @@ return {
                 end)
                 return "<Ignore>"
             end, { expr = true, buffer = bufnr, desc = "Jump to next hunk" })
+
             vim.keymap.set({ "n", "v" }, "[c", function()
                 if vim.wo.diff then
                     return "[c"
