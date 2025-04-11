d0ab1c879229211e44813c61a8f168d9a384edd3
zakwan hisham <hishamzakwan@gmail.com>
2025-04-12 04:17:02 +0800

    remove icons in `oil.lua`

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,13 +1,12 @@
 return {
     "stevearc/oil.nvim",
     lazy = false,
-    dependencies = { require("mini.icons").setup {} },
     cmd = "Oil",
     keys = { { "<leader>-", "<cmd>Oil<cr>", desc = "Oil" } },
     opts = {
         default_file_explorer = true,
         watch_for_changes = true,
-        columns = { "permissions", "size", "birthtime", "icon" },
+        columns = { "permissions", "size", "birthtime"},
         view_options = { show_hidden = true, case_insensitive = true },
         keymaps = { ["q"] = "actions.close" },
     },
