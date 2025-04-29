196b88688249f0188e0c5b1f77f86e9e03e01c6b
zakwan hisham <hishamzakwan@gmail.com>
2025-04-30 02:35:36 +0800

    Use conda and lazy load it
    Only calling conda when needed. This will speed up the fish shell when
    opening new shell/instance

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -64,6 +64,22 @@ abbr ghostrc "nvim $HOME/.config/ghostty/config"
 abbr tconf "nvim $HOME/.tmux.conf"
 abbr vimrc "vim $HOME/.vimrc"
 
+### CONDA SOURCE
+
+set -x CONDA_PATH /data/miniconda3/bin/conda $HOME/miniconda3/bin/conda
+
+function conda
+    functions --erase conda
+    for conda_path in $CONDA_PATH
+        if test -f $conda_path
+            eval $conda_path "shell.fish" "hook" | source
+            conda $argv
+            return
+        end
+    end
+    echo "No conda installation found in $CONDA_PATH"
+end
+
 ### PROMPT AND COMPLETIONS
 
 fzf --fish | source
