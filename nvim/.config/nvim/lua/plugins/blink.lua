5edc0b828f393ebbc6f9a33e74fec715e9564ed1
zakwan hisham <hishamzakwan@gmail.com>
2025-05-10 03:05:40 +0800

    clean up file

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -5,18 +5,9 @@ return {
     version = "*",
     opts = {
         keymap = { preset = "enter" },
-        appearance = {
-            use_nvim_cmp_as_default = true,
-            nerd_font_variant = "mono",
-        },
         completion = {
             list = { selection = { preselect = false, auto_insert = false } },
-            menu = {
-                draw = {
-                    columns = { { "label", gap = 1 }, { "kind", gap = 1 } },
-                    treesitter = { "lsp" },
-                },
-            },
+            menu = { draw = { columns = { { "label", gap = 0 }, { "kind", gap = 0 } } } },
             documentation = { auto_show = true, auto_show_delay_ms = 200 },
             ghost_text = { enabled = false },
         },
