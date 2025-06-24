70eba7590b0160c457a0ae9315c5552c21ab6536
zakwan hisham <hishamzakwan@gmail.com>
2025-06-24 09:46:14 +0800

    Add config to undotree

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -2,4 +2,9 @@ return {
     "mbbill/undotree",
     cmd = "UndotreeToggle",
     keys = { { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Undotree" } },
+    config = function()
+        vim.g.undotree_SetFocusWhenToggle = 1
+        vim.g.undotree_SplitWidth = 50
+        vim.g.undotree_DiffpanelHeight = 15
+    end,
 }
