400b4b54fef9a96d9baa96dea436bede7a9a28ff
zakwan hisham <hishamzakwan@gmail.com>
2025-07-03 01:46:30 +0800

    Sort opts

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -7,7 +7,7 @@ return {
         default_file_explorer = true,
         watch_for_changes = true,
         columns = { "permissions", "size", "birthtime" },
-        view_options = { show_hidden = true, case_insensitive = true },
         keymaps = { ["q"] = "actions.close" },
+        view_options = { show_hidden = true, case_insensitive = true },
     },
 }
