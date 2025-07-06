395e477d67324a6597d58f53c0cf4a098cf0c0e4
zakwan hisham <hishamzakwan@gmail.com>
2025-07-06 13:50:40 +0800

    Update the colorscheme config

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,9 +5,11 @@ return {
     config = function()
         vim.g.gruvbox_material_background = "hard"
         vim.g.gruvbox_material_foreground = "original"
+        vim.g.gruvbox_material_enable_bold = 1
         vim.g.gruvbox_material_dim_inactive_windows = 1
         vim.g.gruvbox_material_ui_contrast = "high"
         vim.g.gruvbox_material_float_style = "dim"
+        vim.g.gruvbox_material_statusline_style = "original"
         vim.g.gruvbox_material_better_performance = 1
 
         vim.cmd.colorscheme "gruvbox-material"
