0aa2343ea848a0d68783fde346ad15c570c39b2d
zakwan hisham <hishamzakwan@gmail.com>
2025-07-07 16:17:38 +0800

    disable bold
    disabling bold as this mess up my eyes

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,7 +5,6 @@ return {
     config = function()
         vim.g.gruvbox_material_background = "hard"
         vim.g.gruvbox_material_foreground = "original"
-        vim.g.gruvbox_material_enable_bold = 1
         vim.g.gruvbox_material_dim_inactive_windows = 1
         vim.g.gruvbox_material_ui_contrast = "high"
         vim.g.gruvbox_material_float_style = "dim"
