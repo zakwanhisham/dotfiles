7160de333e7164728011ca287b99f803d0d77d66
zakwan hisham <hishamzakwan@gmail.com>
2025-07-10 23:21:25 +0800

    remove augment, a hassle with the code suggestions

--- editor/.config/nvim/lua/plugins/augment.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "augmentcode/augment.vim",
-    lazy   = false,
-    cmd    = "Augment",
-    config = function()
-        vim.g.augment_workspace_folders = { os.getenv("HOME") .. "/Work/landing/" }
-    end
-}
