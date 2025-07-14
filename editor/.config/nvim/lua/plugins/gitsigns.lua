5d86abe3a1164c43164adb96d1ce6041cdd5c310
zakwan hisham <hishamzakwan@gmail.com>
2025-07-14 08:14:19 +0800

    Update gitsigns keymaps

--- editor/.config/nvim/lua/plugins/gitsigns.lua
+++ editor/.config/nvim/lua/plugins/gitsigns.lua
@@ -7,7 +7,12 @@ return {
 
             vim.keymap.set("n", "<leader>gp", require("gitsigns").preview_hunk,
                 { buffer = bufnr, desc = "Preview git hunk" })
-            vim.keymap.set("n", "<leader>gb", require("gitsigns").blame, { buffer = bufnr, desc = "Git blame" })
+            vim.keymap.set("n", "<leader>gb", require("gitsigns").blame,
+                { buffer = bufnr, desc = "Git blame" })
+            vim.keymap.set("n", "<leader>gl", require("gitsigns").blame_line,
+                { buffer = bufnr, desc = "Git blame line" })
+            vim.keymap.set("n", "<leader>gt", require("gitsigns").toggle_current_line_blame,
+                { buffer = bufnr, desc = "Toggle current line blame" })
 
             vim.keymap.set({ "n", "v" }, "]c", function()
                 if vim.wo.diff then
