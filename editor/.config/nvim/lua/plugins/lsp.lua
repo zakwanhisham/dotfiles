98f1ef5c0025e6f0758fced81aeecf5449587421
zakwan hisham <hishamzakwan@gmail.com>
2025-07-26 22:31:21 +0800

    Clean up lsp autocommand

--- editor/.config/nvim/lua/plugins/lsp.lua
+++ editor/.config/nvim/lua/plugins/lsp.lua
@@ -51,30 +51,6 @@ return {
                 nmap("<leader>wl", function()
                     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
                 end, "Workspace List Folders")
-
-                local client = vim.lsp.get_client_by_id(event.data.client_id)
-                if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-                    local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = true })
-                    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-                        buffer = event.buf,
-                        group = highlight_augroup,
-                        callback = vim.lsp.buf.document_highlight,
-                    })
-
-                    vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-                        buffer = event.buf,
-                        group = highlight_augroup,
-                        callback = vim.lsp.buf.clear_references,
-                    })
-
-                    vim.api.nvim_create_autocmd("LspDetach", {
-                        group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
-                        callback = function(event2)
-                            vim.lsp.buf.clear_references()
-                            vim.api.nvim_clear_autocmds { group = "lsp-highlight", buffer = event2.buf }
-                        end,
-                    })
-                end
             end,
         })
 
