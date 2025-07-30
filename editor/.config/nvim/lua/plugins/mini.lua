599ef073b0a25d0ca10c67e7d5eb18830a7db231
zakwan hisham <hishamzakwan@gmail.com>
2025-07-31 01:11:40 +0800

    Remove unnecessary plugin

--- editor/.config/nvim/lua/plugins/mini.lua
+++ editor/.config/nvim/lua/plugins/mini.lua
@@ -55,41 +55,11 @@ return {
         end
     },
     {
-        "echasnovski/mini.comment",
-        event = "VeryLazy",
-        version = false,
-        config = function()
-            require("mini.comment").setup {
-                mappings = {
-                    -- SOLUTION: https://stackoverflow.com/questions/9051837/how-to-map-c-to-toggle-comments-in-vim
-                    comment_line = "<C-_>",   -- this equate to "<C-/>"
-                    comment_visual = "<C-_>", -- this equate to "<C-/>"
-                },
-            }
-        end
-    },
-    {
         "echasnovski/mini.ai",
         event = "VeryLazy",
         version = false,
         config = function()
             require("mini.ai").setup { n_lines = 500 }
-        end
-    },
-    {
-        "echasnovski/mini.indentscope",
-        event = "VeryLazy",
-        version = false,
-        config = function()
-            require("mini.indentscope").setup {}
-        end
-    },
-    {
-        "echasnovski/mini.move",
-        event = "VeryLazy",
-        version = false,
-        config = function()
-            require("mini.move").setup {}
         end
     },
     {
