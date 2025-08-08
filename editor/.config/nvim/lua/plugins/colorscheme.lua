7dcc7f338bf5a43b75d4b3aee2980781111f9e34
zakwan hisham <hishamzakwan@gmail.com>
2025-08-09 03:48:24 +0800

    Clean up files

--- editor/.config/nvim/lua/plugins/colorscheme.lua
+++ editor/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,12 +2,14 @@ local add, now = MiniDeps.add, MiniDeps.now
 
 add { source = "sainnhe/gruvbox-material" }
 
-vim.g.gruvbox_material_background = "hard"
-vim.g.gruvbox_material_foreground = "original"
-vim.g.gruvbox_material_dim_inactive_windows = 1
-vim.g.gruvbox_material_ui_contrast = "high"
-vim.g.gruvbox_material_float_style = "dim"
-vim.g.gruvbox_material_statusline_style = "original"
-vim.g.gruvbox_material_better_performance = 1
+now(function()
+    vim.g.gruvbox_material_background = "hard"
+    vim.g.gruvbox_material_foreground = "original"
+    vim.g.gruvbox_material_dim_inactive_windows = 1
+    vim.g.gruvbox_material_ui_contrast = "high"
+    vim.g.gruvbox_material_float_style = "dim"
+    vim.g.gruvbox_material_statusline_style = "original"
+    vim.g.gruvbox_material_better_performance = 1
 
-now(function() vim.cmd.colorscheme "gruvbox-material" end)
+    vim.cmd.colorscheme "gruvbox-material"
+end)
