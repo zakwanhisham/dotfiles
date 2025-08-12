ff76ae46b3e6f3be4dd3f245d71fca58fb04470d
zakwan hisham <hishamzakwan@gmail.com>
2025-08-12 23:04:43 +0800

    Use thick git signs

--- editor/.config/nvim/lua/plugins/gitsigns.lua
+++ editor/.config/nvim/lua/plugins/gitsigns.lua
@@ -13,6 +13,22 @@ now(function()
 
     local gitsigns = require("gitsigns")
     gitsigns.setup {
+        signs = {
+            add          = { text = '▒' },
+            change       = { text = '▒' },
+            delete       = { text = '▒' },
+            topdelete    = { text = '▒' },
+            changedelete = { text = '▒' },
+            untracked    = { text = '▒' },
+        },
+        signs_staged = {
+            add          = { text = '▒' },
+            change       = { text = '▒' },
+            delete       = { text = '▒' },
+            topdelete    = { text = '▒' },
+            changedelete = { text = '▒' },
+            untracked    = { text = '▒' },
+        },
         on_attach = function(bufnr)
             local gs = package.loaded.gitsigns
 
