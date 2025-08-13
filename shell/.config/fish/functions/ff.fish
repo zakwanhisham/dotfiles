ea8d6caa2fc68da25ae388d68c7f3205dee4ad05
zakwan hisham <hishamzakwan@gmail.com>
2025-08-13 14:36:47 +0800

    Update ff.fish

--- shell/.config/fish/functions/ff.fish
+++ shell/.config/fish/functions/ff.fish
@@ -1,7 +1,7 @@
 function ff -d "Find files and cd into it"
     set selected_dir (fd --hidden -L --exclude .git --type directory . "$HOME" | fzf --header "Directory Selection" --height 40%)
     if test -n "$selected_dir"
-        printf "Moving to %s\n" "$selected_dir"
+        printf "Moving to `%s`\n" "$selected_dir"
         cd "$selected_dir"
     else
         echo "No directory selected."
