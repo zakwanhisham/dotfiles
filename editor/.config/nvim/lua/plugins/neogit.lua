40a34c7311e381a1a0c9454c02072ca349c0b350
zakwan hisham <hishamzakwan@gmail.com>
2025-08-22 17:07:01 +0800

    Revert "Use vim fugitive and remove neogit"
    This reverts commit d2be2e4d17a8ac1b1169aefecc380cbe95a36ecf.

--- editor/.config/nvim/lua/plugins/neogit.lua
+++ editor/.config/nvim/lua/plugins/neogit.lua
@@ -1,6 +1,6 @@
 local add, later = MiniDeps.add, MiniDeps.later
 
-add { source = "tpope/vim-fugitive" }
+add { source = "NeogitOrg/neogit", depends = { "nvim-lua/plenary.nvim" } }
 
 later(function()
     local nmap = function(keymap, command, desc)
@@ -11,6 +11,5 @@ later(function()
         vim.keymap.set("n", keymap, command, { desc = desc })
     end
 
-    nmap("<leader>gl", "<cmd>tab Git log --graph %<cr>", "Log")
-    nmap("<leader>gb", "<cmd>Git blame<cr>", "Blame")
+    nmap("<leader>gg", "<cmd>Neogit<cr>", "Neogit")
 end)
