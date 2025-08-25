d0b4513985d65a39a412e62efc912f3a8e733ec5
zakwan hisham <hishamzakwan@gmail.com>
2025-08-26 01:44:02 +0800

    Use `dim` float style

--- editor/.config/nvim/lua/plugins/colorscheme.lua
+++ editor/.config/nvim/lua/plugins/colorscheme.lua
@@ -9,7 +9,7 @@ now(function()
     vim.g.gruvbox_material_enable_italic = 1
     vim.g.gruvbox_material_dim_inactive_windows = 1
     vim.g.gruvbox_material_ui_contrast = "high"
-    vim.g.gruvbox_material_float_style = "bright"
+    vim.g.gruvbox_material_float_style = "dim"
     vim.g.gruvbox_material_disable_terminal_colors = 1
     vim.g.gruvbox_material_statusline_style = "original"
     vim.g.gruvbox_material_better_performance = 1
