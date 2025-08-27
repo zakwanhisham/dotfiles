098b82fc4820f63904e736410599700dabed1a76
zakwan hisham <hishamzakwan@gmail.com>
2025-08-28 01:51:57 +0800

    Use high ui contrast

--- editor/.config/nvim/lua/plugins/colorscheme.lua
+++ editor/.config/nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,7 @@ now(function()
     vim.g.gruvbox_material_background = "hard"
     vim.g.gruvbox_material_foreground = "original"
     vim.g.gruvbox_material_enable_bold = 1
+    vim.g.gruvbox_material_ui_contrast = "high"
     vim.g.gruvbox_material_disable_terminal_colors = 1
     vim.g.gruvbox_material_better_performance = 1
 
