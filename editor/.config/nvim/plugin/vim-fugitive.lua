bfb172ce27b7f2464e0c0be90445ae8fef76098e
zakwan hisham <hishamzakwan@gmail.com>
2025-08-30 03:15:53 +0800

    Use fugitive as git client

--- editor/.config/nvim/plugin/vim-fugitive.lua
+++ editor/.config/nvim/plugin/vim-fugitive.lua
@@ -11,6 +11,7 @@ later(function()
         vim.keymap.set("n", keymap, command, { desc = desc })
     end
 
+    nmap("<leader>gg", "<cmd>tab Git<cr>", "Fugitive")
     nmap("<leader>gl", "<cmd>tab Git log --graph %<cr>", "Log")
     nmap("<leader>gb", "<cmd>Git blame<cr>", "Blame")
     nmap("<leader>gd", "<cmd>Gdiffsplit<cr>", "Diff")
