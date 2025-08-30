53b917793435d40eff2acd9f7d7e5d2f17bd711a
zakwan hisham <hishamzakwan@gmail.com>
2025-08-31 03:45:36 +0800

    House Keeping

--- editor/.config/nvim/plugin/neogit.lua
+++ editor/.config/nvim/plugin/neogit.lua
@@ -3,7 +3,13 @@ local add, later = MiniDeps.add, MiniDeps.later
 add { source = "NeogitOrg/neogit", depends = { "nvim-lua/plenary.nvim" } }
 
 later(function()
-    require("neogit").setup {}
+    local nmap = function(keymap, command, desc)
+        if desc then
+            desc = "Git: " .. desc
+        end
 
-    vim.keymap.set("n", "<leader>gg" ,"<cmd>Neogit<cr>", { desc = "Git: Neogit" })
+        vim.keymap.set("n", keymap, command, { desc = desc })
+    end
+
+    nmap("<leader>gg", "<cmd>Neogit<cr>", "Neogit")
 end)
