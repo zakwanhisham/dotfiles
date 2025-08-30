3908cebb549785773c19dc654da5bde59ce609e8
zakwan hisham <hishamzakwan@gmail.com>
2025-08-31 04:32:28 +0800

    Use fzf.vim and ivy profiles

--- editor/.config/nvim/plugin/fzf.lua
+++ editor/.config/nvim/plugin/fzf.lua
@@ -11,19 +11,13 @@ later(function()
         vim.keymap.set("n", keymap, command, { desc = desc })
     end
 
-    nmap("<leader><space>", "<cmd>FzfLua buffers<cr>", "Buffers")
-    nmap("<leader>ff", "<cmd>FzfLua files<cr>", "Files")
-    nmap("<leader>fs", "<cmd>FzfLua grep<cr>", "Grep")
+    nmap("<leader><space>", "<cmd>Buffers<cr>", "Buffers")
+    nmap("<leader>ff", "<cmd>Files<cr>", "Files")
+    nmap("<leader>fs", "<cmd>Rg<cr>", "Grep")
     nmap("<leader>fw", "<cmd>FzfLua grep_cword<cr>", "Word")
-    nmap("<leader>fm", "<cmd>FzfLua marks<cr>", "Marks")
+    nmap("<leader>fm", "<cmd>Marks<cr>", "Marks")
     nmap("<leader>fr", "<cmd>FzfLua resume<cr>", "Resume")
-    nmap("<leader>/", "<cmd>FzfLua grep_curbuf<cr>", "Search Buffer")
+    nmap("<leader>/", "<cmd>BLines<cr>", "Buffer Lines")
 
-    require("fzf-lua").setup {
-        defaults = { file_icons = false },
-        file_ignore_patterns = { "%.git/.*", "node_modules/.*" },
-        winopts = { split = "botright new" },
-        files = { cwd_prompt = false },
-        grep = { cmd = "rg --vimgrep --column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e" },
-    }
+    require("fzf-lua").setup { { "fzf-vim", "ivy" } }
 end)
