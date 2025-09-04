a78b44c48b22ba9a6458dfa66662e2d50bbb6086
zakwan hisham <hishamzakwan@gmail.com>
2025-09-04 14:43:28 +0800

    Disable inline suggest and open on right on 80

--- editor/.config/nvim/plugin/amazonq.lua
+++ editor/.config/nvim/plugin/amazonq.lua
@@ -3,5 +3,15 @@ local add, now = MiniDeps.add, MiniDeps.now
 add { source = "awslabs/amazonq.nvim" }
 
 now(function()
-    require("amazonq").setup { ssoStartUrl = "https://view.awsapps.com/start" }
+    require("amazonq").setup {
+        ssoStartUrl = "https://view.awsapps.com/start",
+        inline_suggest = false,
+        on_chat_open = function()
+            vim.cmd [[
+                vertical botright split
+                vertical resize 80
+                set wrap breakindent nonumber norelativenumber nolist signcolumn=no
+            ]]
+        end
+    }
 end)
