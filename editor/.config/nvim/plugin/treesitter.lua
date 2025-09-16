60c8b863ef1205106359a394d102dc19874bada6
zakwan hisham <hishamzakwan@gmail.com>
2025-09-17 03:46:04 +0800

    Remove treesitter-context

--- editor/.config/nvim/plugin/treesitter.lua
+++ editor/.config/nvim/plugin/treesitter.lua
@@ -2,14 +2,12 @@ local add, later = MiniDeps.add, MiniDeps.later
 
 add {
     source = "nvim-treesitter/nvim-treesitter",
-    depends = { "nvim-treesitter/nvim-treesitter-context" },
     checkout = "master",
     monitor = "main",
     hooks = { post_checkout = function() vim.cmd [[ TSUpdate ]] end },
 }
 
 later(function()
-    require("treesitter-context").setup { max_lines = 5 }
     require("nvim-treesitter.install").prefer_git = true
 
     ---@diagnostic disable-next-line: missing-fields
