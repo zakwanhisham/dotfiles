ee7c75edb18c57d14578258d7abb0cc208796e36
zakwan hisham <hishamzakwan@gmail.com>
2025-09-18 18:13:26 +0800

    Disable border for blink

--- editor/.config/nvim/plugin/blink.lua
+++ editor/.config/nvim/plugin/blink.lua
@@ -22,7 +22,7 @@ later(function()
         keymap = { preset = "enter" },
         completion = {
             list = { selection = { preselect = false, auto_insert = false } },
-            menu = { draw = { columns = { { "label", gap = 0 }, { "kind", gap = 0 } } } },
+            menu = { border = "none", draw = { columns = { { "label", gap = 0 }, { "kind", gap = 0 } } } },
             documentation = { auto_show = true, auto_show_delay_ms = 200 },
             ghost_text = { enabled = false },
         },
