ddcddb5cf83dfb73322609cb80016c7e22df75eb
zakwan hisham <hishamzakwan@gmail.com>
2025-09-18 12:44:03 +0800

    Disable dropbar on certain buffer

--- editor/.config/nvim/plugin/dropbar.lua
+++ editor/.config/nvim/plugin/dropbar.lua
@@ -3,5 +3,42 @@ local add, later = MiniDeps.add, MiniDeps.later
 add { source = "Bekaboo/dropbar.nvim" }
 
 later(function()
-    require("dropbar").setup {}
+    require("dropbar").setup {
+        bar = {
+            enable = function(buf, win, _)
+                buf = vim._resolve_bufnr(buf)
+                if
+                    not vim.api.nvim_buf_is_valid(buf)
+                    or not vim.api.nvim_win_is_valid(win)
+                then
+                    return false
+                end
+
+                if
+                    not vim.api.nvim_buf_is_valid(buf)
+                    or not vim.api.nvim_win_is_valid(win)
+                    or vim.fn.win_gettype(win) ~= ''
+                    or vim.wo[win].winbar ~= ''
+                    or vim.bo[buf].ft == 'help'
+                    or vim.bo[buf].ft == 'terminal'
+                    or vim.bo[buf].ft == 'gitcommit'
+                    or vim.bo[buf].ft == 'NeogitDiffView'
+                then
+                    return false
+                end
+
+                local stat = vim.uv.fs_stat(vim.api.nvim_buf_get_name(buf))
+                if stat and stat.size > 1024 * 1024 then
+                    return false
+                end
+
+                return vim.bo[buf].ft == 'markdown'
+                    or pcall(vim.treesitter.get_parser, buf)
+                    or not vim.tbl_isempty(vim.lsp.get_clients({
+                        bufnr = buf,
+                        method = vim.lsp.protocol.Methods.textDocument_documentSymbol,
+                    }))
+            end
+        }
+    }
 end)
