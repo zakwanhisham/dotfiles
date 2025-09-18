038e3dd94c7bbc444122e7f6d92ab58936ba6211
zakwan hisham <hishamzakwan@gmail.com>
2025-09-18 17:33:28 +0800

    Clean up file

--- editor/.config/nvim/plugin/12_autocommand.lua
+++ editor/.config/nvim/plugin/12_autocommand.lua
@@ -41,7 +41,6 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     callback = function(event)
         if event.match:match "^%w%w+:[\\/][\\/]" then return end
-        ---@diagnostic disable-next-line: undefined-field
         local file = vim.uv.fs_realpath(event.match) or event.match
         vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
     end,
