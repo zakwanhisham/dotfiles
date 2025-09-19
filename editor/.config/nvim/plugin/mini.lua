1904f63535594656f4c29c6ecb93fc9f0e6da355
zakwan hisham <hishamzakwan@gmail.com>
2025-09-19 17:21:12 +0800

    Update statusline and variable name

--- editor/.config/nvim/plugin/mini.lua
+++ editor/.config/nvim/plugin/mini.lua
@@ -1,17 +1,16 @@
 local now, later = MiniDeps.now, MiniDeps.later
 
 now(function()
-    require("mini.statusline").setup {
+    local MiniStatusline = require("mini.statusline")
+    MiniStatusline.setup {
         content = {
             active = function()
-                local MiniStatusline = require("mini.statusline")
-
-                local diagnostics    = MiniStatusline.section_diagnostics { trunc_width = 75 }
-                local diff           = MiniStatusline.section_diff { trunc_width = 75 }
-                local filename       = MiniStatusline.section_filename { trunc_width = 140 }
-                local git            = "%{FugitiveStatusline()}"
-                local highlight      = "MiniStatuslineModeNormal"
-                local location       = MiniStatusline.section_location { trunc_width = 75 }
+                local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
+                local diff        = MiniStatusline.section_diff { trunc_width = 75 }
+                local filename    = MiniStatusline.section_filename { trunc_width = 140 }
+                local git         = "%{FugitiveStatusline()}"
+                local highlight   = "MiniStatuslineModeNormal"
+                local location    = MiniStatusline.section_location { trunc_width = 75 }
 
                 vim.cmd [[
                     hi MiniStatuslineModeNormal cterm=NONE ctermfg=234 ctermbg=246 gui=NONE guifg=#1d2021 guibg=#a89984
@@ -27,6 +26,15 @@ now(function()
                     { hl = highlight, strings = { location } },
                 }
             end,
+            inactive = function()
+                local filename  = MiniStatusline.section_filename { trunc_width = 140 }
+                local highlight = "MiniStatuslineInactive"
+
+                return MiniStatusline.combine_groups {
+                    "%<",
+                    { hl = highlight, strings = { filename } },
+                }
+            end,
         },
         use_icons = false,
     }
@@ -34,19 +42,19 @@ end)
 
 later(function()
     require("mini.ai").setup {}
-    local miniclue = require('mini.clue')
-    miniclue.setup {
+    local MiniClue = require('mini.clue')
+    MiniClue.setup {
         clues = {
             { mode = "n", keys = "<leader>f", desc = "+Fzf" },
             { mode = "n", keys = "<leader>g", desc = "+Git" },
             { mode = "n", keys = "<leader>w", desc = "+Whitespace" },
-            miniclue.gen_clues.square_brackets(),
-            miniclue.gen_clues.builtin_completion(),
-            miniclue.gen_clues.g(),
-            miniclue.gen_clues.marks(),
-            miniclue.gen_clues.registers(),
-            miniclue.gen_clues.windows(),
-            miniclue.gen_clues.z(),
+            MiniClue.gen_clues.square_brackets(),
+            MiniClue.gen_clues.builtin_completion(),
+            MiniClue.gen_clues.g(),
+            MiniClue.gen_clues.marks(),
+            MiniClue.gen_clues.registers(),
+            MiniClue.gen_clues.windows(),
+            MiniClue.gen_clues.z(),
         },
         triggers = {
             { mode = 'n', keys = '<Leader>' },
@@ -75,8 +83,8 @@ later(function()
         view = { style = "sign" },
         mappings = { goto_first = '[C', goto_prev = '[c', goto_next = ']c', goto_last = ']C' }
     }
-    local hipatterns = require("mini.hipatterns")
-    hipatterns.setup { highlighters = { hipatterns.gen_highlighter.hex_color() } }
+    local MiniHipatterns = require("mini.hipatterns")
+    MiniHipatterns.setup { highlighters = { MiniHipatterns.gen_highlighter.hex_color() } }
     require("mini.indentscope").setup {}
     require("mini.move").setup {}
     require("mini.surround").setup {}
