be6c2cb01ece852a8127b4e935fbc9b9b4782aa9
zakwan hisham <hishamzakwan@gmail.com>
2025-09-23 16:57:38 +0800

    Use `high` float style highlight

--- editor/.config/nvim/plugin/colorscheme.lua
+++ editor/.config/nvim/plugin/colorscheme.lua
@@ -8,7 +8,7 @@ now(function()
     vim.g.gruvbox_material_background = "hard"
     vim.g.gruvbox_material_foreground = "original"
     vim.g.gruvbox_material_disable_italic_comment = 1
-    vim.g.gruvbox_material_float_style = "none"
+    vim.g.gruvbox_material_float_style = "high"
     vim.g.gruvbox_material_disable_terminal_colors = 1
     vim.g.gruvbox_material_statusline_style = "original"
     vim.g.gruvbox_material_better_performance = 1
