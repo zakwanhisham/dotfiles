b121cb45d8c317dcc4abf82cbfea0b80340f16c3
zakwan hisham <hishamzakwan@gmail.com>
2025-10-01 16:25:17 +0800

    Simplify custom MiniStatuslineModeNormal highlight

--- editor/.config/nvim/after/plugin/mini.lua
+++ editor/.config/nvim/after/plugin/mini.lua
@@ -12,9 +12,7 @@ now(function()
                 local highlight   = "MiniStatuslineModeNormal"
                 local location    = MiniStatusline.section_location { trunc_width = 75 }
 
-                vim.cmd [[
-                    hi MiniStatuslineModeNormal cterm=NONE ctermfg=234 ctermbg=246 gui=NONE guifg=#1d2021 guibg=#a89984
-                ]]
+                vim.cmd [[ hi MiniStatuslineModeNormal cterm=NONE gui=NONE ]]
 
                 return MiniStatusline.combine_groups {
                     "%<",
