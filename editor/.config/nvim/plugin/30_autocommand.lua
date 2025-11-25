e11137a02e84e7304dabac552a34e0afe5ab8c95
zakwan hisham <hishamzakwan@gmail.com>
2025-11-25 23:09:53 +0800

    Add autoresize autocommand

--- editor/.config/nvim/plugin/30_autocommand.lua
+++ editor/.config/nvim/plugin/30_autocommand.lua
@@ -24,10 +24,18 @@ vim.api.nvim_create_autocmd("FileType", {
     callback = function() vim.b.miniindentscope_disable = true end,
 })
 
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+vim.api.nvim_create_autocmd("BufWritePre", {
     callback = function(event)
         if event.match:match "^%w%w+:[\\/][\\/]" then return end
         local file = vim.uv.fs_realpath(event.match) or event.match
         vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
     end,
+})
+
+vim.api.nvim_create_autocmd("VimResized", {
+    callback = function()
+        local current_tab = vim.fn.tabpagenr()
+        vim.cmd("tabdo wincmd =")
+        vim.cmd("tabnext " .. current_tab)
+    end
 })
