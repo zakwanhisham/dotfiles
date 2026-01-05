8099a158c8d1b265c60f56b0d511094349580c55
zakwan hisham <hishamzakwan@gmail.com>
2026-01-05 14:25:23 +0800

    Use DP-0 and update xrandr

--- screen/.screenlayout/monitor.sh
+++ screen/.screenlayout/monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output HDMI-A-0 --rate 75 --mode 2560x1080 --pos 0x0 --rotate normal
+xrandr --output DisplayPort-0 --rate 200 --mode 3440x1440 --pos 0x0 --rotate normal --output DisplayPort-1 --off --output HDMI-A-0 --off
