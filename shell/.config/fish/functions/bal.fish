7b2e5df56cfc5d93f171eaef3482977bfa4288fa
zakwan hisham <hishamzakwan@gmail.com>
2026-01-09 23:37:24 +0800

    Remove powerprofile alias

--- shell/.config/fish/functions/bal.fish
+++
@@ -1,4 +0,0 @@
-function bal --wraps=powerprofilesctl\ set\ balanced\;\ and\ echo\ \'Set\ power\ to\ Balanced\' --description alias\ bal\ powerprofilesctl\ set\ balanced\;\ and\ echo\ \'Set\ power\ to\ Balanced\'
-  powerprofilesctl set balanced; and echo 'Set power to Balanced' $argv
-        
-end
--- shell/.config/fish/functions/saver.fish
+++
@@ -1,4 +0,0 @@
-function saver --wraps=powerprofilesctl\ set\ power-saver\;\ and\ echo\ \'Set\ power\ to\ Power-Saver\' --description alias\ saver\ powerprofilesctl\ set\ power-saver\;\ and\ echo\ \'Set\ power\ to\ Power-Saver\'
-  powerprofilesctl set power-saver; and echo 'Set power to Power-Saver' $argv
-        
-end
