be43e1adfad39952890940341d79c988bba0499a
zakwan hisham <hishamzakwan@gmail.com>
2026-01-19 22:42:42 +0800

    Inline things

--- editor/.config/nvim/plugin/60_plugins.lua
+++ editor/.config/nvim/plugin/60_plugins.lua
@@ -72,9 +72,7 @@ now_if_args(function()
 
             local client = vim.lsp.get_client_by_id(event.data.client_id)
             if client == nil then return end
-            if client.name == "ruff" then
-                client.server_capabilities.hoverProvider = false
-            end
+            if client.name == "ruff" then client.server_capabilities.hoverProvider = false end
         end
     })
 
