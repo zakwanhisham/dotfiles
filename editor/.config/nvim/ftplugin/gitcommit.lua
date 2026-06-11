5c45aea9e4a9c4d1fd4f4fb04b5332ec33a52bbd
zakwan hisham <hishamzakwan@gmail.com>
2026-06-12 01:27:31 +0800

    Fix gitcommit not respecting ftplugin runtimepath

--- editor/.config/nvim/ftplugin/gitcommit.lua
+++ editor/.config/nvim/ftplugin/gitcommit.lua
@@ -1,1 +1,4 @@
-vim.cmd [[ setlocal colorcolumn=50 spell wrap signcolumn=no foldmethod=expr foldexpr=v:lua.MiniGit.diff_foldexpr() foldlevel=1 ]]
+vim.schedule(function()
+    vim.treesitter.stop()
+    vim.cmd [[ setlocal colorcolumn=50 textwidth=72 spell wrap signcolumn=no foldmethod=expr foldexpr=v:lua.MiniGit.diff_foldexpr() foldlevel=1 ]]
+end)
