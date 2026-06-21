83427217e941c8b0b9b22bf7246efde642727d16
zakwan hisham <hishamzakwan@gmail.com>
2026-06-22 00:37:35 +0800

    Add colorcolum=72 to gitcommit

--- editor/.config/nvim/ftplugin/gitcommit.lua
+++ editor/.config/nvim/ftplugin/gitcommit.lua
@@ -1,4 +1,4 @@
 vim.schedule(function()
     vim.treesitter.stop()
-    vim.cmd [[ setlocal colorcolumn=50 textwidth=72 spell wrap signcolumn=no foldmethod=expr foldexpr=v:lua.MiniGit.diff_foldexpr() foldlevel=1 ]]
+    vim.cmd [[ setlocal colorcolumn=50,72 textwidth=72 spell wrap signcolumn=no foldmethod=expr foldexpr=v:lua.MiniGit.diff_foldexpr() foldlevel=1 ]]
 end)
