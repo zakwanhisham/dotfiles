c09a3915ddf55d3ea629f9abed4d01ffd50ae110
zakwan hisham <hishamzakwan@gmail.com>
2026-07-07 09:21:41 +0800

    Add startinsert when commiting

--- editor/.config/nvim/ftplugin/gitcommit.lua
+++ editor/.config/nvim/ftplugin/gitcommit.lua
@@ -1,4 +1,7 @@
 vim.schedule(function()
     vim.treesitter.stop()
-    vim.cmd [[ setlocal colorcolumn=50,72 textwidth=72 spell wrap signcolumn=no foldmethod=expr foldexpr=v:lua.MiniGit.diff_foldexpr() foldlevel=1 ]]
+    vim.cmd [[
+        startinsert
+        setlocal colorcolumn=50,72 textwidth=72 spell wrap signcolumn=no foldmethod=expr foldexpr=v:lua.MiniGit.diff_foldexpr() foldlevel=1
+    ]]
 end)
