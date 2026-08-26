e0004455cdf311fbe66d99b09f699c2330cbf6b3
zakwan hisham <hishamzakwan@gmail.com>
2026-08-26 23:02:22 +0800

    Use no color theme

---
+++ shell/.config/fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,45 @@
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+
+set --global fish_color_autosuggestion brblack
+set --global fish_color_cancel 
+set --global fish_color_command 
+set --global fish_color_comment 
+set --global fish_color_cwd --reset
+set --global fish_color_cwd_root --reset
+set --global fish_color_end 
+set --global fish_color_error 
+set --global fish_color_escape 
+set --global fish_color_gray 
+set --global fish_color_history_current 
+set --global fish_color_host --reset
+set --global fish_color_host_remote yellow
+set --global fish_color_keyword 
+set --global fish_color_normal --reset
+set --global fish_color_operator 
+set --global fish_color_option 
+set --global fish_color_param 
+set --global fish_color_quote 
+set --global fish_color_redirection 
+set --global fish_color_search_match --reverse
+set --global fish_color_selection --reverse
+set --global fish_color_status --reset
+set --global fish_color_user --reset
+set --global fish_color_valid_path 
+set --global fish_pager_color_background 
+set --global fish_pager_color_completion --reset
+set --global fish_pager_color_description brblack
+set --global fish_pager_color_prefix --underline=single
+set --global fish_pager_color_progress brblack
+set --global fish_pager_color_secondary_background 
+set --global fish_pager_color_secondary_completion 
+set --global fish_pager_color_secondary_description 
+set --global fish_pager_color_secondary_prefix 
+set --global fish_pager_color_selected_background --reverse
+set --global fish_pager_color_selected_completion 
+set --global fish_pager_color_selected_description 
+set --global fish_pager_color_selected_prefix 
